93ba490b4549a08fff3dff731bd8e7cbbb8d40e9
trevor4n <trevor4n@gmail.com>
2021-09-17 12:20:41 -0500

    api liftoff ���

--- src/Components/TravelSafe.js
+++ src/Components/TravelSafe.js
@@ -3,8 +3,21 @@ import React, { useEffect } from 'react'
 const TravelSafe = () => {
 
     useEffect(() => {
-        
-            fetch('http://localhost:8000/proxy')
+        const requestOptions = {
+            method: 'GET', // default
+            // method: 'POST', // default
+            // mode: 'no-cors', // todo - uncomment?
+            redirect: 'follow' // default
+          }
+
+            fetch('http://localhost:8000/proxy/country_code/DE/', requestOptions, {
+                headers: {
+                    'Authorization': 'ApiKey ' + process.env.REACT_APP_TRAVEL_SAFE_KEY,
+                    'Accept': 'application/json',
+                    'Api-Version': '1',
+                    'Accept-Language': 'en'
+                }
+            })
             .then(res => res.json())
             .then(res => console.log(res))
         
